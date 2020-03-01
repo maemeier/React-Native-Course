71146c215ea0deaa5f6e235f80b630ee29a1a71d
Mae Kangsadan <maemeier@live.com>
2020-03-01 18:34:31 +0100

    set all assets

---
+++ App.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Text, StyleSheet, View, ScrollView } from "react-native";
+import styled from "styled-components";
+import Card from "./components/Card";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Title>Welcome back,</Title>
+          <Name>Mae</Name>
+
+          <Subtitle>Continue Learning</Subtitle>
+        </TitleBar>
+        <ScrollView
+          horizontal={true}
+          style={{ paddingBottom: 30 }}
+          showsHorizontalScrollIndicator={false}
+        >
+          <Card
+            title="Styled Components"
+            image={require("./assets/background13.jpg")}
+            caption="React Native"
+            logo={require("./assets/logo-react.png")}
+            subtitle="5 of 12 sections"
+          />
+          <Card
+            title="Props and Children"
+            image={require("./assets/background14.jpg")}
+            caption="React JS"
+            logo={require("./assets/logo-react.png")}
+            subtitle="10 of 12 sections"
+          />
+        </ScrollView>
+      </Container>
+    );
+  }
+}
+
+const Container = styled.View`
+  background: #f0f3f5;
+  flex: 1;
+`;
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
