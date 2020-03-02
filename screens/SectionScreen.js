18c67ea230fe4c8bd265fd2f6e1f5cd1598296ce
Mae Kangsadan <maemeier@live.com>
2020-03-02 21:37:49 +0100

    hidden status bar

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "react-native";
+import { TouchableOpacity, StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 class SectionScreen extends React.Component {
   static navigationOptions = {
@@ -11,7 +12,22 @@ class SectionScreen extends React.Component {
     const section = navigation.getParam("section");
     return (
       <Container>
-        <Text>{section.title}</Text>
+        <StatusBar hidden />
+        <Cover>
+          <Image source={section.image} />
+          <Title>{section.title}</Title>
+          <Caption>{section.caption}</Caption>
+        </Cover>
+        <TouchableOpacity style={{ position: "absolute", top: 20, right: 20 }}>
+          <CloseView>
+            <Ionicons
+              name="ios-close"
+              size={36}
+              color="#546cfb"
+              style={{ marginTop: -2 }}
+            />
+          </CloseView>
+        </TouchableOpacity>
       </Container>
     );
   }
@@ -21,8 +37,40 @@ export default SectionScreen;
 
 const Container = styled.View`
   flex: 1;
+`;
+
+const Cover = styled.View`
+  height: 375px;
+`;
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  width: 170px;
+  position: absolute;
+  top: 78px;
+  left: 20px;
+`;
+const Caption = styled.Text`
+  color: white;
+  font-size: 17px;
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
+  width: 300px;
+`;
+
+const CloseView = styled.View`
+  width: 32px;
+  height: 32px;
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
   justify-content: center;
   align-items: center;
 `;
-
-const Text = styled.Text``;
