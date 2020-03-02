bb2fc1c20405a6c69a8278797378368475cc7e7d
Mae Kangsadan <maemeier@live.com>
2020-03-02 18:34:03 +0100

    Thank God! nagivation works!

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,2 +1,15 @@
-import { createStackNavigator, createAppContainer } from "react-navigator";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
 import HomeScreen from "../screens/HomeScreen";
+
+// stack navigator
+
+const screens = {
+  HomeScreen: {
+    screen: HomeScreen
+  }
+};
+const AppNavigator = createStackNavigator(screens);
+
+export default createAppContainer(AppNavigator);
