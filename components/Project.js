44984a1f43574852de330bc2ce3c58bf1424a728
Mae Kangsadan <maemeier@live.com>
2020-03-04 04:57:38 +0100

    pan x and y

---
+++ components/Project.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+class Project extends React.Component {
+  render() {
+    return <Container />;
+  }
+}
+
+export default Project;
+const Container = styled.View`
+  width: 315px;
+  height: 460px;
+  border-radius: 14px;
+  background-color: blue;
+`;
--- screens/ProjectScreen.js
+++ screens/ProjectScreen.js
@@ -1,20 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import Project from "../components/Project";
 import styled from "styled-components";
+import { PanResponder, Animated } from "react-native";
 
-const ProjectScreen = props => {
-  return (
-    <Cover>
-      <Text> Project components</Text>
-    </Cover>
-  );
-};
+class ProjectScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+  state = {
+    pan: new Animated.ValueXY()
+  };
+
+  componentWillMount() {
+    this._panresponder = PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPandResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.pan.x,
+          dy: this.state.pan.y
+        }
+      ])
+    });
+  }
+  render() {
+    return (
+      <Container>
+        <Animated.View
+          style={{
+            transform: [
+              { translateX: this.state.pan.x },
+              { translateY: this.state.pan.y }
+            ]
+          }}
+        >
+          <Project />
+        </Animated.View>
+      </Container>
+    );
+  }
+}
 
 export default ProjectScreen;
 
-const Cover = styled.View`
-  height: 142px;
-  background: pink;
+const Container = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
+  background: #f0f3f5;
 `;
+
+const Text = styled.Text``;
